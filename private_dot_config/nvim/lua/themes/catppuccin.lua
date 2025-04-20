ac857d2e0601412ed4118dfc8c7d87a24a253d6e
Arne Wauters <arne.wauters@gmail.com>
2025-04-20 12:28:39 +0200

    switch theme

--- private_dot_config/nvim/lua/themes/catppuccin.lua
+++ private_dot_config/nvim/lua/themes/catppuccin.lua
@@ -2,16 +2,16 @@ local function is_dark_mode()
     local result = vim.fn.systemlist('defaults read -g AppleInterfaceStyle')
     return result[1] == 'Dark'
   end
-  
-  local flavour = (is_dark_mode() and 'latte' or 'mocha')
-  
-  require('catppuccin').setup({ 
+
+  local flavour = (is_dark_mode() and 'mocha' or 'latte')
+
+  require('catppuccin').setup({
       integrations = {
         gitsigns = true,
         nvimtree = true,
         telescope = true,
     },
-    flavour = flavour, 
+    flavour = flavour,
     transparent_background = true })
-  
+
   vim.cmd.colorscheme('catppuccin')
